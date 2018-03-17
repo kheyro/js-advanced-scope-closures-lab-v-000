a4208830dac0802e3809a2edb6e8aa10bff9190f
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 15:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
-    let numBlock = Math.abs(startBlock.substring(-2) - endBlock.substring(-2))
+    let numBlock = Math.abs(startBlock.slice(-2) - endBlock.sslice(-2))
     console.log(startBlock.substring(-2))
     if (numBlock > blockRange) {
       return `${numBlock} out of range`
