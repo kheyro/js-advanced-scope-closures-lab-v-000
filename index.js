cad20d9366b6d0451fb9d5da0acd8e45573beafd
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-17 15:18:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
+    let numBlock = Math.abs(startBlock.slice(0, -2) - endBlock.slice(0, -2))
+    if (numBlock > blockRange) {
+      return `${numBlock - blockRange} blocks out of range`
+    } else {
+      return `within range by ${numBlock}`
+    }
+  }
+}
+
+function produceTipCalculator(percent){
+  return function(tip) {
+    return tip * percent
+  }
+}
